ad4233367793de6b03d424c09ca8ae19c9121f18
Alexander Danylchenko <olexander.danilchenko@gmail.com>
2020-02-04 23:46:31 +0200

    Completed update method for employees

--- dbUpdate.c
+++ dbUpdate.c
@@ -25,28 +25,31 @@ void updateShopEmployeeId(shop changeShop, int newEmployeeId) {
   changeShop.employeeId = newEmployeeId;
 
   // TODO Fix doubling
-  FILE *shopDataFile;
-  openDbFile(&shopDataFile, shopsData);
+  FILE *shopsDataFile;
+  openDbFile(&shopsDataFile, shopsData);
   unsigned long shopAddress = getAddressByKey(changeShop.id, shopsData);
-  fseek(shopDataFile, (long)shopAddress, SEEK_SET);
-  fwrite(&changeShop, sizeof(shop), 1, shopDataFile);
-  fclose(shopDataFile);
+  fseek(shopsDataFile, (long)shopAddress, SEEK_SET);
+  fwrite(&changeShop, sizeof(shop), 1, shopsDataFile);
+  fclose(shopsDataFile);
 }
 
-// TODO
 void updateS() {
     employee updEmployee = getS();
-    if (updShop.isActive) {
-        printf("\\\\ Enter new address: ");
+    if (updEmployee.isActive) {
+        printf("\\\\ Enter new first name: ");
         fflush(stdin);
-        gets(updShop.address);
+        gets(updEmployee.firstName);
+
+        printf("\\\\ Enter new last name: ");
+        fflush(stdin);
+        gets(updEmployee.lastName);
 
         // TODO Fix doubling
-        FILE *shopDataFile;
-        openDbFile(&shopDataFile, shopsData);
-        unsigned long shopAddress = getAddressByKey(updShop.id, shopsData);
-        fseek(shopDataFile, (long)shopAddress, SEEK_SET);
-        fwrite(&updShop, sizeof(shop), 1, shopDataFile);
-        fclose(shopDataFile);
+        FILE *employeesDataFile;
+        openDbFile(&employeesDataFile, employeesData);
+        unsigned long employeeAddress = getAddressByKey(updEmployee.id, employeesData);
+        fseek(employeesDataFile, (long)employeeAddress, SEEK_SET);
+        fwrite(&updEmployee, sizeof(shop), 1, employeesDataFile);
+        fclose(employeesDataFile);
     }
 }
